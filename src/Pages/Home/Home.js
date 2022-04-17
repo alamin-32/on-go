2d4ce475fd0de72c9737d782aa76b1eb750c96b3
alamin-32 <alaminhussain32@gmail.com>
2022-04-17 20:02:13 +0600

    carosoul added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import Services from "../ServicesPage/Services/Services";
+import caro1 from "../../images/caro1.png";
+import caro2 from "../../images/caro2.png";
+import caro3 from "../../images/caro3.png";
+import caro4 from "../../images/caro4.png";
+import caro5 from "../../images/caro5.png";
 
 const Home = () => {
   return (
@@ -8,22 +13,14 @@ const Home = () => {
       <div>
         <Carousel fade>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://i.ibb.co/z5jJzhf/pic3.png"
-              alt="First slide"
-            />
+            <img className="d-block w-100" src={caro1} alt="First slide" />
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://i.ibb.co/QM7kQyR/pic2.png"
-              alt="Second slide"
-            />
+            <img className="d-block w-100" src={caro2} alt="Second slide" />
 
             <Carousel.Caption>
               <h3>Second slide label</h3>
@@ -31,11 +28,7 @@ const Home = () => {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://i.ibb.co/RQ5vMLL/pic4.png"
-              alt="Third slide"
-            />
+            <img className="d-block w-100" src={caro3} alt="Third slide" />
 
             <Carousel.Caption>
               <h3>Third slide label</h3>
@@ -45,11 +38,17 @@ const Home = () => {
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://i.ibb.co/WWHHmth/pic1.png"
-              alt="Third slide"
-            />
+            <img className="d-block w-100" src={caro4} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Fourth slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={caro5} alt="Third slide" />
 
             <Carousel.Caption>
               <h3>Fourth slide label</h3>
